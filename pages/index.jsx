f578f9f5898ac82d619f5aacddc12965af674259
Your Name <you@example.com>
2025-02-03 20:09:59 +0100

    Hello world

---
+++ pages/index.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import Admin from "./components/Admin";
+// import picture from "./public/speed-neon-lights-city.jpg";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center mt-10">
+      <Admin />
+    </div>
+  );
+}
